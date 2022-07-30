efc23a02e6de36ed42af531e21d63e50775df624
Noe Zavaleta C <nzavaletac@gmail.com>
2022-07-29 23:24:42 -0500

    Listo el renderizado de todas las peliculas

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 const Listado = () => {
   const token = localStorage.getItem("token");
@@ -9,34 +9,39 @@ const Listado = () => {
 
   useEffect(() => {
     const endPoint =
-      "https://api.themoviedb.org/3/discover/movie?api_key=660f0ca35415ef05ecdf4390fab6ac9d&language=es_ES&sort_by=popularity.desc";
+      "https://api.themoviedb.org/3/discover/movie?api_key=660f0ca35415ef05ecdf4390fab6ac9d&language=es-ES&sort_by=popularity.desc";
     axios.get(endPoint).then((response) => {
       const apiData = response.data;
       setMoviesList(apiData.results);
     });
   }, [setMoviesList]);
 
-  console.log(moviesList);
-
   return (
     <>
       {!token && <Navigate to="/" />}
       <div className="row">
-        <div className="col-3">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <Link to="/" className="btn btn-primary">
-                Go somewhere
-              </Link>
+        {moviesList.map((oneMovie, i) => {
+          return (
+            <div className="col-3" key={i}>
+              <div className="card">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{oneMovie.title}</h5>
+                  <p className="card-text">
+                    {oneMovie.overview.substring(0, 100)}...
+                  </p>
+                  <Link to="/" className="btn btn-primary">
+                    View Detail
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </>
   );
